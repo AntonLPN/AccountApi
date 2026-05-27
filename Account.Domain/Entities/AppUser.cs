7bf083879e307f26423b6328004b7f10a977e27a
AntonLPN <tohapowerlifting1986@gmail.com>
2026-05-27 10:32:55 +0300

    Refactor AppUser and ApiKey entities for improved relationships and nullable properties

--- Account.Domain/Entities/AppUser.cs
+++ Account.Domain/Entities/AppUser.cs
@@ -1,9 +1,16 @@
 
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace Account.Domain.Entities;
 
-public class AppUser:IdentityUser
+public class AppUser
 {
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+    public bool EmailConfirmed { get; set; }
+    public string? PasswordHash { get; set; }
     public ICollection<ApiKey> ApiKeys { get; set; } = [];
 }
--- Account.Infrastructure/Persistence/AppDbContext.cs
+++ Account.Infrastructure/Persistence/AppDbContext.cs
@@ -4,13 +4,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Account.Infrastructure.Persistence;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<AppUser>(options)
+public class AppDbContext() : DbContext
 {
+    public DbSet<AppUser> AppUsers { get; set; } = null!;
     public DbSet<ApiKey> ApiKeys { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+
+        builder.Entity<AppUser>(entity =>
+        {
+            entity.HasKey(u => u.Id).HasName("PK_AppUser");
+            entity.Property(e => e.UserName).HasMaxLength(255).HasColumnName("UserName").IsUnicode();
+            entity.Property(e => e.Email).HasMaxLength(255).HasColumnName("Email").IsUnicode();
+            entity.Property(e => e.PasswordHash).HasMaxLength(255).HasColumnName("PasswordHash").IsUnicode();
+            entity.Property(e => e.EmailConfirmed).HasColumnName("EmailConfirmed").HasDefaultValue(false);
+
+            entity.HasIndex(u => u.Email).IsUnique();
+        });
+
         builder.Entity<ApiKey>(entity =>
         {
             entity.HasKey(a => a.Id).HasName("PK_ApiKey");
@@ -20,10 +34,12 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbCo
             entity.Property(e => e.CreatedAt).HasColumnName("CreatedAt");
             entity.Property(e => e.ExpiredAt).HasColumnName("ExpiredAt");
 
+            // Один пользователь -> много ApiKey (настройка на стороне зависимой сущности ApiKey)
             entity.HasOne(a => a.AppUser)
                 .WithMany(u => u.ApiKeys)
                 .HasForeignKey(a => a.UserId)
-                .HasConstraintName("FK_ApiKey_AppUser");
+                .HasConstraintName("FK_AppUser_ApiKeys")
+                .OnDelete(DeleteBehavior.Cascade); // или SetNull / Restrict — в зависимости от логики
         });
     }
 }
