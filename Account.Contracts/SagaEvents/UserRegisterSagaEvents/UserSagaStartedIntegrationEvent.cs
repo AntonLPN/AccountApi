206a2afdbe4dceac722308f2576f25e323c0d5c4
AntonLPN <tohapowerlifting1986@gmail.com>
2026-05-30 21:55:07 +0300

    Add `Account.Contracts` project with UserSagaStartedIntegrationEvent definition

---
+++ Account.Contracts/SagaEvents/UserRegisterSagaEvents/UserSagaStartedIntegrationEvent.cs
@@ -0,0 +1,8 @@
+namespace Account.Contracts.SagaEvents.UserRegisterSagaEvents;
+
+public class UserSagaStartedIntegrationEvent
+{
+    public Guid CorrelationId { get; init; }
+    public string UserId { get; init; } = null!;
+    public string Email { get; init; } = null!;
+}
