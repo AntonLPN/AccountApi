d7e5c756800b2cd457bf2c9d70da926b0de74618
AntonLPN <tohapowerlifting1986@gmail.com>
2026-05-30 22:01:12 +0300

    Add EF configuration for `UserRegistrationSagaState` in AppDbContext

--- Account.Infrastructure/Persistence/AppDbContext.cs
+++ Account.Infrastructure/Persistence/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Account.Domain.Entities;
+using Account.Infrastructure.Persistence.SagaModels;
 using Microsoft.EntityFrameworkCore;
 
 namespace Account.Infrastructure.Persistence;
@@ -7,6 +8,7 @@ public class AppDbContext : DbContext
 {
     public DbSet<AppUser> AppUsers { get; set; } = null!;
     public DbSet<ApiKey> ApiKeys { get; set; } = null!;
+    public DbSet<UserRegistrationSagaState> UserRegistrationSagaStates { get; set; } = null!;
 
     // ReSharper disable once ConvertToPrimaryConstructor
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
@@ -44,6 +46,21 @@ public class AppDbContext : DbContext
                 .HasForeignKey(a => a.UserId)
                 .HasConstraintName("FK_AppUser_ApiKeys")
                 .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        builder.Entity<UserRegistrationSagaState>(entity =>
+        {
+            entity.HasKey(s => s.CorrelationId);
+            entity.Property(s => s.CorrelationId).HasMaxLength(255).HasColumnName("CorrelationId").IsUnicode();
+            entity.Property(s => s.CurrentState).HasMaxLength(255).HasColumnName("CurrentState").IsUnicode();
+            entity.Property(x => x.UserId).HasMaxLength(255).HasColumnName("UserId").IsUnicode();
+            entity.Property(x => x.Email).HasMaxLength(255).HasColumnName("Email").IsUnicode();
+            entity.Property(x => x.ApiKey).HasMaxLength(255).HasColumnName("ApiKey").IsUnicode();
+            entity.Property(x => x.CreatedAt).HasColumnName("CreatedAt");
+            entity.Property(x => x.UpdatedAt).HasColumnName("UpdatedAt");
+            entity.Property(x => x.EmailConfirmationSent).HasColumnName("EmailConfirmationSent").HasDefaultValue(false);
+            entity.Property(x => x.ProfileInitialized).HasColumnName("ProfileInitialized").HasDefaultValue(false);
+            entity.Property(x => x.FailureReason).HasMaxLength(255).HasColumnName("FailureReason").IsUnicode();
         });
     }
 }
