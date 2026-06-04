adef7ea5c69ec35175f789610defa8820f12aee8
AntonLPN <tohapowerlifting1986@gmail.com>
2026-06-04 17:32:43 +0300

    Add implementation send welcome email in consumer

--- Account.Infrastructure/Consumers/SendEmailConfirmationConsumer.cs
+++ Account.Infrastructure/Consumers/SendEmailConfirmationConsumer.cs
@@ -1,20 +1,32 @@
 using Account.Contracts.SagaEvents.UserRegisterSagaEvents.Commands;
 using Account.Contracts.SagaEvents.UserRegisterSagaEvents.Events;
+using Account.Domain.Interfaces;
+using Account.Domain.ValueObjects;
 using MassTransit;
 using Microsoft.Extensions.Logging;
 
 namespace Account.Infrastructure.Consumers;
 
-public class SendEmailConfirmationConsumer(ILogger<SendEmailConfirmationConsumer> logger)
+public class SendEmailConfirmationConsumer(ILogger<SendEmailConfirmationConsumer> logger,IEmail emailService)
     : IConsumer<SendEmailConfirmationIntegrationEvent>
 {
-    public Task Consume(ConsumeContext<SendEmailConfirmationIntegrationEvent> context)
+    public async Task Consume(ConsumeContext<SendEmailConfirmationIntegrationEvent> context)
     {
-        //TODO implement logic for sending email confirmation, for now just simulating with log and publish next event in saga
+        var res = await emailService.SendEmail(context.Message.Email, "WelcomeTemplate.html");
+        if (!res)
+        {
+            await context.Publish(new UserRegistrationSagaFailedIntegrationEvent
+            {
+                CorrelationId = context.Message.CorrelationId,
+                UserId = context.Message.UserId,
+                FailureReason = "Failed to send welcome email"
+            });
+            return;
+        }
         logger.LogInformation(
             "Consumed SendEmailConfirmationCommandIntegrationEvent: UserId={UserId}, Email={Email}",
-            context.Message.UserId, context.Message.Email);
-        return context.Publish(new EmailConfirmationSentIntegrationEvent
+            context.Message.UserId, MaskedEmail.Create(context.Message.Email));
+        await context.Publish(new EmailConfirmationSentIntegrationEvent
         {
             CorrelationId = context.Message.CorrelationId,
             UserId = context.Message.UserId
