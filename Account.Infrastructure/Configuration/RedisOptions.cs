17a34f6c4c7c6a7a575321344d565b4cfa870567
AntonLPN <tohapowerlifting1986@gmail.com>
2026-06-27 17:46:03 +0300

    Refactor RedisOptions: change properties to use init-only setters for better immutability

--- Account.Infrastructure/Configuration/RedisOptions.cs
+++ Account.Infrastructure/Configuration/RedisOptions.cs
@@ -2,10 +2,10 @@ namespace Account.Infrastructure.Configuration;
 
 public class RedisOptions
 {
-    public string Host { get; set; }
-    public int Port { get; set; }
-    public string Password { get; set; }
-    public string User { get; set; }
-    public bool Ssl { get; set; }
-    public int CacheStorageTime { get; set; }
+    public string Host { get; init; } = null!;
+    public int Port { get; init; }
+    public string Password { get; init; } = null!;
+    public string User { get; init; } = null!;
+    public bool Ssl { get; init; }
+    public int CacheStorageTime { get; init; }
 }
