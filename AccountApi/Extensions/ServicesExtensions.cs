93ae7cd269a70cb32d89849ad73cbe9f00911f21
AntonLPN <tohapowerlifting1986@gmail.com>
2026-08-11 12:51:30 +0300

    Refactoring Redis extensions

--- AccountApi/Extensions/ServicesExtensions.cs
+++ AccountApi/Extensions/ServicesExtensions.cs
@@ -34,28 +34,4 @@ public static class ServicesExtensions
 
         return services;
     }
-
-    public static IServiceCollection AddRedis(this IServiceCollection services, IConfiguration configuration)
-    {
-        var redisSection = configuration.GetSection("Redis").Get<RedisOptions>() ??
-                           throw new InvalidOperationException("Redis configuration section is missing.");
-        var redisOptions = new ConfigurationOptions
-        {
-            EndPoints = { { redisSection.Host, redisSection.Port } },
-            User = redisSection.User,
-            Password = redisSection.Password,
-            Ssl = redisSection.Ssl,
-            AbortOnConnectFail = false,
-            ConnectTimeout = 5000
-        };
-
-        var redis = ConnectionMultiplexer.Connect(redisOptions);
-        if (!redis.IsConnected)
-            throw new InvalidOperationException("Failed to connect to Redis");
-
-        services.AddSingleton<IConnectionMultiplexer>(redis);
-        services.AddStackExchangeRedisCache(options => { options.ConfigurationOptions = redisOptions; });
-
-        return services;
-    }
 }
