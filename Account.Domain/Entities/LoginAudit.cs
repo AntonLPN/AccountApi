06e3d691e9d9bae63365e777273fc9c3571b9fe7
AntonLPN <tohapowerlifting1986@gmail.com>
2026-09-22 17:13:09 +0300

    Refactor ApiKey and LoginAudit to use GuardClauses for input validation

--- Account.Domain/Entities/LoginAudit.cs
+++ Account.Domain/Entities/LoginAudit.cs
@@ -1,13 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 using Account.Domain.DTOs;
+using Ardalis.GuardClauses;
 
 namespace Account.Domain.Entities;
 
 public class LoginAudit : AggregateRoot
 {
-    [Key]
-    public int Id { get; set; }
-    public Guid UserId { get; set; } 
+    [Key] public int Id { get; set; }
+    public Guid UserId { get; set; }
     public string Email { get; set; } = "";
     public string? IpAddress { get; set; }
     public string? UserAgent { get; set; }
@@ -16,17 +16,14 @@ public class LoginAudit : AggregateRoot
 
     public static LoginAudit Create(CreateLoginAuditParams createLoginAuditParams)
     {
-        ArgumentNullException.ThrowIfNull(createLoginAuditParams.UserId, nameof(createLoginAuditParams.UserId));
-        ArgumentException.ThrowIfNullOrWhiteSpace(createLoginAuditParams.Email, nameof(createLoginAuditParams.Email));
         return new LoginAudit
         {
-            UserId = createLoginAuditParams.UserId,
-            Email = createLoginAuditParams.Email,
+            UserId = Guard.Against.Default(createLoginAuditParams.UserId),
+            Email = Guard.Against.NullOrWhiteSpace(createLoginAuditParams.Email),
             IpAddress = createLoginAuditParams.IpAddress,
             UserAgent = createLoginAuditParams.UserAgent,
             IsSuspicious = createLoginAuditParams.IsSuspicious,
-            LoggedInAt = createLoginAuditParams.LoggedInAt
+            LoggedInAt = Guard.Against.Default(createLoginAuditParams.LoggedInAt)
         };
-        
     }
 }
